ef4d1afc7ec3a6008a91b9a80481db1f4a0feab8
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-07-20 11:33:28 +0200

    chapter8

--- ClassExercises/src/main/java/chapter3/Example3_31.java
+++ ClassExercises/src/main/java/chapter3/Example3_31.java
@@ -3,22 +3,22 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package chapter3;
-
-/**
- *
- * @author Jaco
- */
-public class Example3_31 {
-  public static void main(String args[]) { 
- 
-    one: for(int i=0; i<3; i++) { 
-      System.out.print("Pass " + i + ": "); 
-    } 
- 
-    for(int j=0; j<100; j++) { 
-      if(j == 10) break one; // WRONG 
-      System.out.print(j + " "); 
-    } 
-  } 
-}
+//package chapter3;
+//
+///**
+// *
+// * @author Jaco
+// */
+//public class Example3_31 {
+//  public static void main(String args[]) { 
+// 
+//    one: for(int i=0; i<3; i++) { 
+//      System.out.print("Pass " + i + ": "); 
+//    } 
+// 
+//    for(int j=0; j<100; j++) { 
+//      if(j == 10) break one; // WRONG 
+//      System.out.print(j + " "); 
+//    } 
+//  } 
+//}
